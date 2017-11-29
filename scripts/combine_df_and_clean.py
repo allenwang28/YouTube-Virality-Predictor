7f35635f3dad7b89c3d97d13fdd1295686083141
Allen Wang <allenwang28@yahoo.com>
2017-11-29 15:11:24 -0600

    Did some work with GLoVE word embeddings as far as visualizations and clustering. Also started on boolean-based observations.

--- scripts/combine_df_and_clean.py
+++ scripts/combine_df_and_clean.py
@@ -2,12 +2,29 @@ import numpy as np
 import pandas as pd
 import os
 
+from nltk.corpus import words
+
 import re
+
+# this script also removes non-english words by comparing the number of english to non-english words 
+# in the title. Remove samples with less than ENGLISH_PERCENT_THRESH in title.
+ENGLISH_PERCENT_THRESH = 0.6
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 IMG_DIR_PATH = os.path.join(ROOT_DIR, '..', 'images')
 DATA_PATH = os.path.join(ROOT_DIR, '..', 'data')
 
+english_words_set = set(words.words())
+
+def get_percent_english(s):
+    tokens = re.split(' ', s)
+    num_english = 0
+
+    for token in tokens:
+        if token in english_words_set:
+            num_english += 1
+    return num_english / float(len(tokens))
+    
 def clean(s):
     if len(s) > 3:
         if s[0] == 'b':
@@ -44,9 +61,13 @@ df = df[df['Id'] != '#NAME?']
 # Drop the duplicated indices
 df.drop_duplicates(subset='Id', keep='last')
 
+# Remove entries where it's non-english
+
 print ('DF shape: {0}'.format(df.shape))
 
 df = pd.merge(df, df_nsfw, on='Id')
+
+#df['PercentEnglishInTitle'] = df['Title'].apply(get_percent_english)
 
 print ('Merged DF shape: {0}'.format(df.shape))
 
